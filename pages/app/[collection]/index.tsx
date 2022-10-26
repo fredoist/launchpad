f8e8ec234e55af85958b542077a848acbfda9d9f
Freddy González <7684330+fredoist@users.noreply.github.com>
2022-10-26 18:11:26 -0500

    Add collection dashboard

---
+++ pages/app/[collection]/index.tsx
@@ -0,0 +1,20 @@
+import { Layout } from 'components/Layout';
+import { NextPage } from 'next';
+import Link from 'next/link';
+
+const Collection: NextPage = () => {
+  return (
+    <Layout title="Stage">
+      <section className="pt-32 pb-12">
+        <h1 className="text-3xl font-bold">Stage for app</h1>
+      </section>
+      <section className='flex gap-2 border-b border-black/5'>
+        <Link href="">
+          <a className='py-1 px-2 border-b-2 border-black'>Collection</a>
+        </Link>
+      </section>
+    </Layout>
+  );
+}
+
+export default Collection;
--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -9,7 +9,7 @@ const App: NextPage = () => {
         <h1 className="text-3xl font-bold">Dashboard</h1>
       </section>
       <section className="grid grid-cols-4 gap-5">
-        <Link href="/app/stage" passHref>
+        <Link href="/app/collection" passHref>
           <article className="p-4 ring-1 ring-black/5 rounded-xl hover:ring-black/20 flex flex-col gap-2 cursor-pointer">
             <h3 className="font-semibold">Collection Name</h3>
             <span>A collection description</span>
