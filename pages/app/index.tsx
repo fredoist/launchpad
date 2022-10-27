1741c43c85fc256dbf93b447dbf25a5cb612511b
Freddy González <7684330+fredoist@users.noreply.github.com>
2022-10-26 20:28:35 -0500

    Fix links

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -22,7 +22,7 @@ const App: NextPage = () => {
       </section>
       <section className="grid grid-cols-4 gap-5">
         {collections.map((collection) => (
-          <Link key={collection.programAddress} href="/app" as={`/app/${collection.programAddress}`}>
+          <Link key={collection.programAddress} href={`/app/${collection.programAddress}`}>
             <article className="p-4 ring-1 ring-black/5 rounded-xl hover:ring-black/20 flex flex-col gap-2 cursor-pointer">
               <h3 className="font-semibold">{collection.programName}</h3>
             </article>
