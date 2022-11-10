f7798824e38075c3408dfdf9b72ec931b4275b81
Freddy González <luisalfredogonzalezr@gmail.com>
2022-11-10 06:48:46 +0000

    Update banner announcement styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,47 +3,26 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { sidebar } from 'stores/sidebar';
 import Confetti from 'react-confetti';
-import { useEffect, useState } from 'react';
+import { useWindowSize } from 'react-use';
 
 export const Header = () => {
   const wallet = useWallet();
 
   /** Temporary add react-confetti effect */
-  const [size, setSize] = useState({ width: 0, height: 0 });
-  useEffect(() => {
-    if (
-      typeof window !== undefined &&
-      localStorage.getItem('confetti') !== 'seen'
-    ) {
-      setSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-      localStorage.setItem('confetti', 'seen');
-    }
-  }, []);
+  const { width, height } = useWindowSize();
 
   return (
     <header className="fixed top-0 inset-x-0 z-10 bg-white/50 backdrop-blur">
-      <Confetti width={size.width} height={size.height} recycle={false} />
-      <div className="text-center p-1 bg-blue-700 text-white text-sm">
-        Launchpad ended up as 2nd place for Solanathon! 🎉{' '}
+      <div className="text-center p-2 bg-blue-700 text-white text-sm relative overflow-hidden">
+        <Confetti width={width} height={height} opacity={0.8} />
+        🎉 Launchpad won 2nd place for Solanathon!
         <a
           href="https://twitter.com/thirdweb/status/1590101360925495296"
           target="_blank"
           rel="noopener noreferrer"
-          className="underline"
+          className="underline uppercase ml-2 font-semibold"
         >
           see announcement
-        </a>
-        {' or '}
-        <a
-          href="https://fredoist.substack.com/subscribe"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline"
-        >
-          subscribe for updates
         </a>
       </div>
       <nav className="mx-auto max-w-6xl flex items-center justify-between py-4 px-5">
